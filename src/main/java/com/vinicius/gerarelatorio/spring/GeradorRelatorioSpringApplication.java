a83afa9893d235fc3ffddd73b9e4a6947086da83
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-10-22 18:27:28 -0300

    Criando projeto parar gerar relatorio (Primeiro commit)

---
+++ src/main/java/com/vinicius/gerarelatorio/spring/GeradorRelatorioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.gerarelatorio.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorRelatorioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorRelatorioSpringApplication.class, args);
+	}
+
+}
